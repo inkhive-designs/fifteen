619f4bdbe883124af4d04c86f6478107cb918336
Gourav Tyagi <gourav@inkhive.com>
2017-09-29 12:34:16 +0530

    Added skins, featured areas, typography, social icons styles.

---
+++ framework/customizer/post_degree.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gourav
+ * Date: 9/28/2017
+ * Time: 11:41 AM
+ */
---
+++ modules/content/_content-single.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gourav
+ * Date: 9/27/2017
+ * Time: 3:50 PM
+ */
